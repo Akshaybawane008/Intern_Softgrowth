1594a2906c3604aa7a52e0d65e418909eae4ede5
sxhil.d3v <jrsahil24@gmail.com>
2025-09-02 18:29:00 +0530

    new  connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import bodyParser from "express";
-
+import mongoose from "mongoose";
+import useRouter from "./Routes/user.js"
 import cors from "cors"
 const app = express()
 app.use(cors({
@@ -11,9 +12,20 @@ app.use(cors({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
+//   server port number
+const port = 4000;
 
-// Routes
-app.use("/api/user", userRouters);
+// mongodb database connection
+mongoose.connect("mongodb+srv://jrsahil24:ECG7sbLpbFnWigbv@cluster0.1rd532c.mongodb.net/",
+    { dbName: "intern_Task_management" }
+).then(() => console.log("mongo db connected successfully")).catch((error) => console.log(error = error.message));
 
-// Start server
-app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+app.use("/api/user/",useRouter);
+
+app.get("/", (req, res) => {
+    res.end("hello sahil")
+})
+
+
+
+app.listen(port, () => console.log(`server is listening on port ${port}`))
